a5be7ea1b8c96b43cf52ceff5b4e0ca34d753cfb
David Tacoronte <davidtacoronte@Davids-MBP.attlocal.net>
2022-09-29 12:50:46 -0400

    completed site per MVP reqs

--- src/index.js
+++ src/index.js
@@ -82,3 +82,17 @@ bottomContent.children[1].children[0].textContent = siteContent['main-content'][
 bottomContent.children[1].children[1].textContent = siteContent['main-content']['product-content'];
 bottomContent.children[2].children[0].textContent = siteContent['main-content']['vision-h4'];
 bottomContent.children[2].children[1].textContent = siteContent['main-content']['vision-content'];
+
+//CTA AREA
+document.querySelector('.cta h1').textContent = siteContent.cta.h1;
+
+document.querySelector('.cta button').textContent = siteContent.cta.button;
+
+//HEADER NAV
+const navLinks = document.querySelectorAll('header nav a');
+const navLinkTexts = Object.values(siteContent.nav);
+navLinks.forEach((link, i) => {
+  link.textContent = navLinkTexts[i]
+  link.classList.add('italic');
+});
+
